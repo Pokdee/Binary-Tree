99cbf7e44ab9bb9ea41d2982017405b01f9063ce
Pokdee <loselife61@gmail.com>
2023-10-27 00:35:21 +0700

    refactor codes

--- script.js
+++ script.js
@@ -394,6 +394,7 @@ class Tree {
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const driver = function () {
+  console.log("The Array for BST", array);
   const tree = new Tree(array);
   tree.isBalanced();
   prettyPrint(tree.root);
